22290480b109d0996fe6996a16670283e3d73bc9
tawnycat <tawnyeleanor@gmail.com>
2017-11-18 00:31:53 -0500

    mostly done, need to fix compatibility math

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,19 +11,39 @@ app.post("/api/friends", function(req, res) {
 var newFriend = req.body;
 friends.push(newFriend);
 
-var newScore = newFriend.scores;
-compatibility(newScore, friends);
+var compatibleFriend = compatibility(newFriend, friends);
+
+res.json(compatibleFriend);
 
 });
 
 };
 
-function compatibility (newScore, friends) {
-	var scoreSum = 0;
-	for (var i = 0; i < newScore.length; i++) {
-		scoreSum + newScore[i];
+function compatibility (newFriend, friends) {
+	console.log(friends);
+
+	var newFriendScore = getScoreSum(newFriend);
+
+	var allScoreSums = friends.map(getScoreSum);
+
+	var indexOfSmallestDiff = 0;
+	for (var i = 0; i < allScoreSums.length; i++) {
+		if (newFriendScore - allScoreSums[i] < newFriendScore - allScoreSums[indexOfSmallestDiff]) {
+			indexOfSmallestDiff = i;
+		}
 	}
-	var 
+	return friends[indexOfSmallestDiff];
+}
+
+function getScoreSum (friend) {
+	console.log(friend);
+
+	var scoreSum = 0;
+	for (var i = 0; i < friend.scores.length; i++) {
+		scoreSum + friend.scores[i];
+	}
+
+	return scoreSum
 }
 
 module.exports = apiRoutes
